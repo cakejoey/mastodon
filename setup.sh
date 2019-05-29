a95e2359df47fd5818a04fe3da584df4c185922b
Joe <josephmalone25@gmail.com>
2019-05-29 11:15:46 -0400

    Create a easy install

---
+++ setup.sh
@@ -0,0 +1,27 @@
+systemctl restart ssh
+apt update && apt upgrade -y
+echo "Enter root password"
+apt install -y curl
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt update
+apt install -y \
+  imagemagick ffmpeg libpq-dev libxml2-dev libxslt1-dev file git-core \
+  g++ libprotobuf-dev protobuf-compiler pkg-config nodejs gcc autoconf \
+  bison build-essential libssl-dev libyaml-dev libreadline6-dev \
+  zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev \
+  nginx redis-server redis-tools postgresql postgresql-contrib \
+  certbot python-certbot-nginx yarn libidn11-dev libicu-dev libjemalloc-dev
+adduser --disabled-login mastodon
+su - mastodon
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+cd ~/.rbenv && src/configure && make -C src
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+exec bash
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+RUBY_CONFIGURE_OPTS=--with-jemalloc rbenv install 2.6.1
+rbenv global 2.6.1
+gem update --system
+gem install bundler --no-document
+exit
